906d6686bbb2a283e4e4d562ec71f47d8e117e8e
Mayur3sh007 <mayureshc007@gmail.com>
2025-06-26 21:52:35 +0530

    Adding Custom application-properties and using them

--- src/main/java/com/firstprojectudemy/firsty/rest/FunRestController.java
+++ src/main/java/com/firstprojectudemy/firsty/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.firstprojectudemy.firsty.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    //Injecting properties from application properties file like .env
+    @Value("${ai.name}")
+    private String aiNAME;
+
+    @Value("${ai.version}")
+    private String aiVERSION;
+
+    @GetMapping("/ai-info")
+    public String getInfo() {
+        return "AI Name: " + aiNAME + " Version: " + aiVERSION;
+    }
+
     // we'll expose a / endpoint to return "Hello World"
     @GetMapping("/")
     public String sayHelloWorld() {
